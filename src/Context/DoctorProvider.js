cb1a140db1cf4602c0893b6329323fd0411dfd45
hieplangsen <hieplangsen@gmail.com>
2023-06-05 08:12:55 +0700

    add code

--- src/Context/DoctorProvider.js
+++ src/Context/DoctorProvider.js
@@ -5,27 +5,40 @@ import { db } from "../Firebase/Firebase.config";
 export const DoctorContext = createContext();
 
 const DoctorProvider = ({ children }) => {
-    const [doctor, setDoctor] = useState([])
-    const getDoctor = async (bv) => {
-      const all = [];
-      let querySnapshot
-      if(bv){
-        const citiesRef = collection(db, "bacsi");
-        const q = query(citiesRef, where("hospital", "==", bv));
-        querySnapshot = await getDocs(q);
-        console.log(querySnapshot, "querySnapshot")
-      } else {
-        querySnapshot = await getDocs(collection(db, "bacsi"));
-      }
-        querySnapshot.docs.forEach((item) => {
-          const row = item.data();
-          row.id = item.id;
-          all.push(row);
-        });
-        setDoctor(all)
-      }
+  const [doctor, setDoctor] = useState([]);
+  const getDoctor = async (bv) => {
+    const all = [];
+    let querySnapshot;
+    if (bv) {
+      const citiesRef = collection(db, "bacsi");
+      const q = query(citiesRef, where("hospital", "==", bv));
+      querySnapshot = await getDocs(q);
+      console.log(querySnapshot, "querySnapshot");
+    } else {
+      querySnapshot = await getDocs(collection(db, "bacsi"));
+    }
+    querySnapshot.docs.forEach((item) => {
+      const row = item.data();
+      row.id = item.id;
+      all.push(row);
+    });
+    setDoctor(all);
+  };
 
-  const doctorInfo = { doctor, getDoctor };
+  const getDoctorByHospital = async (bv) => {
+    const allData = [];
+    let querySnapshot;
+    const citiesRef = collection(db, "users");
+    const q = query(citiesRef, where("hospital", "==", bv));
+    querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      allData.push(doc.data());
+    });
+    setDoctor(allData);
+  };
+
+  const doctorInfo = { doctor, getDoctor, getDoctorByHospital };
 
   return (
     <DoctorContext.Provider value={doctorInfo}>
--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,19 +1,48 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   apiKey: "AIzaSyBU5CcdBgZZrBczIDQbqbF7QSJJCT_fdwg",
-  authDomain: "datlichkhambenh-8b0b2.firebaseapp.com",
-  projectId: "datlichkhambenh-8b0b2",
-  storageBucket: "datlichkhambenh-8b0b2.appspot.com",
-  messagingSenderId: "170451686759",
-  appId: "1:170451686759:web:dbbe68aec3a20f5b188bc2",
-  measurementId: "G-R77XK28D76"
+  apiKey: "AIzaSyBpanVDlbr-hn2sYICdIEDttMyXsoZKsKE",
+  authDomain: "phongkham-ba0e2.firebaseapp.com",
+  projectId: "phongkham-ba0e2",
+  storageBucket: "phongkham-ba0e2.appspot.com",
+  messagingSenderId: "750225861522",
+  appId: "1:750225861522:web:f14654cbaa5fc3d5922f4b",
+  measurementId: "G-D1FQ80FEF0",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-export { app, db }
+
+const uploadFile = async (base64String) => {
+  try {
+    // Tạo một canvas để vẽ ảnh
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    const img = new Image();
+    img.src = base64String;
+    await img.decode(); // Chờ cho ảnh tải xong
+    canvas.width = img.width;
+    canvas.height = img.height;
+    context.drawImage(img, 0, 0);
+
+    // Tải lên tệp tin ảnh lên Firebase Storage
+    const storage = getStorage();
+    const storageRef = ref(storage, `images/${Date.now()}.png`);
+    const blob = await new Promise((resolve) =>
+      canvas.toBlob(resolve, "image/png")
+    );
+    await uploadBytes(storageRef, blob);
+    console.log("Tải lên thành công!");
+
+    // Lấy URL của tệp tin ảnh
+    const url = await getDownloadURL(storageRef);
+    return url;
+  } catch (error) {
+    console.log("Lỗi khi tải lên tệp tin ảnh:", error);
+  }
+};
+export { app, db, uploadFile };
